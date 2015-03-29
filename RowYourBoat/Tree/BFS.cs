8d7e9923704b1a6f42cf10ff2005e6444b136f99
Deliganli <saitkocatas@gmail.com>
2015-03-30 00:57:24 +0300

    Add DFS algorithm

--- RowYourBoat/Tree/BFS.cs
+++ RowYourBoat/Tree/BFS.cs
@@ -30,7 +30,5 @@ namespace RowYourBoat
             }//end-while
             return q;
         }
-
-        
     }
 }
---
+++ RowYourBoat/Tree/DFS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RowYourBoat.Tree
+{
+    class DFS
+    {
+        private static String DISCOVERED = "DISCOVERED";
+        //Breadt First Search
+        public static Stack<Vertex> solve(Vertex vSource, Vertex vDest)
+        {
+            Stack<Vertex> solution = new Stack<Vertex>();
+            solution.Push(vSource);
+            //vSource.Status = DISCOVERED;
+            while (solution.Count > 0)
+            {
+                Vertex current = solution.Pop();
+                if (current.Status != DISCOVERED)
+                {
+                    current.Status = DISCOVERED;
+                    foreach (Edge prime in current.Adjacents)
+                    {
+                        solution.Push(prime.Destination);
+                    }
+                }
+            }//end-while
+            return solution;
+        }
+    }
+}
