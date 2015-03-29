bd45be42ac102a55a03da530ccd214ce69bbd572
Deliganli <saitkocatas@gmail.com>
2015-03-30 00:58:46 +0300

    Cleanup

--- RowYourBoat/RowYourBoat.cs
+++ RowYourBoat/RowYourBoat.cs
@@ -11,59 +11,10 @@ using RowYourBoat;
 namespace RowYourBoat
 {
     public partial class RowYourBoat : Form
-    {
-        List<Char> dangerous;        
-
+    {    
         public RowYourBoat()
         {
             InitializeComponent();
-        }
-
-        private void createDangerousSituations()
-        {
-            dangerous = new List<Char> { };
-            dangerous.Add(Char.SHEEP | Char.CABBAGE);
-            dangerous.Add(Char.WOLF | Char.SHEEP);
-        }
-
-        private void transport(ref List<Char> from, ref List<Char> to, Char character)
-        {
-            if (character != Char.BOATMAN)
-            {
-                from.Remove(character);
-                to.Add(character);
-            }
-
-            from.Remove(Char.BOATMAN);
-            to.Add(Char.BOATMAN);
-        }
-
-        private void solveProblem(List<Char> from, List<Char> to, Stack<Char> moves)
-        {
-            Char current = from.First();
-            transport(ref from, ref to, current);
-            Console.WriteLine(current + " transport attemp");
-
-            if (isDangerous(from)) {
-                transport(ref to, ref from, current);
-                Console.WriteLine(current + " rollback");
-                solveProblem(from, to, moves);
-            }
-            else if (isDangerous(to)) {
-                transport(ref from, ref to, current);
-                Console.WriteLine(current + " rollback");
-                solveProblem(to, from, moves);
-            } else {
-                Console.WriteLine("next iteration");
-                to.Reverse();
-                solveProblem(to, from, moves);
-            }
-        }
-
-        private bool isDangerous(IEnumerable<Char> shore)
-        {
-            var current = shore.Aggregate(new Char(), (a, c) => a | c);
-            return dangerous.Any(situation => current.HasFlag(situation) && !current.HasFlag(Char.BOATMAN));
         }
 
         private void RowYourBoat_Load(object sender, EventArgs e)
@@ -72,51 +23,11 @@ namespace RowYourBoat
             lblPath.Text = String.Empty;
             lblCost.Text = String.Empty;
 
-            createDangerousSituations();
-            List<Char> leftShore = new List<Char> { 
-                Char.WOLF, 
-                Char.SHEEP, 
-                Char.CABBAGE,
-                Char.BOATMAN};
-            List<Char> rightShore = new List<Char> { };
-            Stack<Char> moves = new Stack<Char> { };
-
-            //moves.Push(Characters.BOATMAN);
-            //bool res = isDangerous(leftShore);
-            //solveProblem(leftShore, rightShore, moves);
-
             Graph g = new Graph();
             createTree(g);
             Vertex start = g.GetVertex((Char.BOATMAN | Char.WOLF | Char.SHEEP | Char.CABBAGE).ToString());
             Vertex end = g.GetVertex(Char.NONE.ToString());
             Queue<Vertex> result = BFS.solve(start, end);
-        }
-
-        private void printingSteps(Vertex vSource, Vertex vDest, Graph g)
-        {
-            List<Vertex> stops = g.traceVertex(vSource, vDest);
-            List<Edge> path = g.tracePath(vSource, vDest);
-            try
-            {
-                for (int i = 1; i < stops.Count; i++)
-                {
-                    lblInfo.Text = lblInfo.Text + stops[i].Name + Environment.NewLine;
-                }
-
-                int costTotal = 0;
-
-                for (int i = 0; i < path.Count; i++)
-                {
-                    lblPath.Text = lblPath.Text + path[i].Name + Environment.NewLine;
-                    costTotal = costTotal + path[i].Cost;
-                }
-
-                lblCost.Text = costTotal.ToString();
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.Message);
-            }
         }
 
         private void createTree(Graph g)
